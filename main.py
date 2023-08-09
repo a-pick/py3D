9f15e002f8410851e15a9e49ac60aa54f30dcb74
Alexander Pickering <ajamespickering@gmail.com>
2023-08-09 02:52:43 -0600

    Made vertex size dependent on shape scale and added code comments

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with open("config.toml", "rb") as f:
 
 shapes = [
     Cube(scale=1, position=(-200, -200), color=(0, 255, 0), start_rotation=Rotation(0, 0, 0, "xyz", 0.0025)),
-    Pyramid(scale=2, position=(175, 175), color=(255, 0, 0), start_rotation=Rotation(0, 15, -90, "x", 0.005)),
+    Pyramid(scale=0.25, position=(175, 175), color=(255, 0, 0), start_rotation=Rotation(0, 15, -90, "x", 0.005)),
 ]
 
     
@@ -26,7 +26,7 @@ def clear_terminal():
 
 def project_vertex(vertex):
     """
-    Returns the 2D coordinates of a 3D vertex projected in 2D space
+    Returns the 2D coordinates of a 3D vertex projected in 2D space.
     """
     matmul_res = np.matmul(Projection.projection_matrix, vertex)
     return [matmul_res[0], matmul_res[1]]
@@ -48,19 +48,23 @@ def render(shapes):
 
     for shape in shapes:
         for index, vertex in enumerate(shape.vertices):
+            # Rotate each vertex according to the shape's rotation matrices
             rot_x = np.matmul(shape.start_rotation.x_mat, vertex)
             rot_y = np.matmul(shape.start_rotation.y_mat, rot_x)
             rot_z = np.matmul(shape.start_rotation.z_mat, rot_y)
+            
+            # Project each vertex in 2D space
             vertex_2d = project_vertex(rot_z)
 
+            # Per-shape rotation
             shape.start_rotation.rotate()
             
+            # Scale and translate each vertex
             x = vertex_2d[0] * config['global_scale'] + (window.get_width() / 2) + (shape.position[0] * (config['global_scale'] / 100))
             y = vertex_2d[1] * config['global_scale'] + (window.get_height() / 2) + (shape.position[1] * (config['global_scale'] / 100))
 
             shape.points[index] = (x, y)
-
-            pygame.draw.circle(window, shape.color, (x, y), config['vertex_scale'])
+            pygame.draw.circle(window, shape.color, (x, y), shape.scale * config['vertex_scale'])
         connect_edges(shape)
 
 
@@ -70,6 +74,7 @@ def main():
         window.fill(config['background_color'])
         render(shapes)
 
+        # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
